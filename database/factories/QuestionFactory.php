e060dc28f9210156bb1245a44bceb3b08e6c19e6
MuriloEduardo <muriloeduardoooooo@gmail.com>
2018-10-27 11:22:56 -0300

    editando factory QuestionFactory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -3,7 +3,7 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Question::class, function (Faker $faker) {
-    $title = $faker->unique()->title();
+    $title = $faker->unique()->sentence();
     return [
         'title' => $title,
         'slug' => str_slug($title),
