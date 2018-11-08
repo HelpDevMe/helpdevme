c3c5bd0a1ee1875b29151e7929d99bbc14ff0a83
MuriloEduardo <muriloeduardoooooo@gmail.com>
2018-11-08 13:38:57 -0200

    setando como default 0 para status do post

--- database/migrations/2018_10_30_184042_create_posts_table.php
+++ database/migrations/2018_10_30_184042_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->text('body');
             $table->float('budget')->nullable();
             $table->tinyInteger('type')->default(1);
-            $table->tinyInteger('status');
+            $table->tinyInteger('status')->default(0);
             $table->integer('talk_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->timestamps();
